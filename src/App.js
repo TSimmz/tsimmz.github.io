a4f7e2178e223df3aa63afa69844f1042bb0ee90
Unknown <tyler.simoni.8@gmail.com>
2021-08-30 23:02:19 -0400

    Basic styling/setup

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import React from 'react';
 
+const homepage = {
+  display: "flex",
+  flexFlow: "column",
+  width: "600px",
+  margin: "30px",
+}
+
 function App() {
   return (
-    <div>
+    <div style={homepage}>
       <h1> Tyler Simoni</h1>
-      <hr/>
+      <hr style={{width: "inherit"}}/>
       <p>I'm a frontend developer and designer based in the Tampa Bay.</p>
-      <button>Know more</button>
+      <button style={{width: "100px"}}>Know more</button>
     </div>
   );
 }
