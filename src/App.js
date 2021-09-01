2191207a12c6514076a10fed179782151baff3a8
Unknown <tyler.simoni.8@gmail.com>
2021-08-31 21:15:42 -0400

    Creating starting body and class component

--- src/App.js
+++ src/App.js
@@ -1,21 +1,32 @@
+import './style.css';
 import React from 'react';
+import Navbar from './components/Navbar/Navbar';
+import NavItem from './components/NavItem/NavItem';
+import Footer from './components/Footer/Footer';
 
-const homepage = {
-  display: "flex",
-  flexFlow: "column",
-  width: "600px",
-  margin: "30px",
-}
+class App extends React.Component {
 
-function App() {
-  return (
-    <div style={homepage}>
-      <h1> Tyler Simoni</h1>
-      <hr style={{width: "inherit"}}/>
-      <p>I'm a frontend developer and designer based in the Tampa Bay.</p>
-      <button style={{width: "100px"}}>Know more</button>
-    </div>
-  );
+  render() {
+    return (
+      <>
+        <Navbar>
+          <NavItem title='Home' href='#home'/>
+          <NavItem title='Portfolio' href='#portfolio'/>
+          <NavItem title='About' href='#about'/>
+          <NavItem title='Contact' href='#contact'/>
+        </Navbar> 
+        <main>
+          <section id="home">
+            <h1> Tyler Simoni</h1>
+            <hr/>
+            <p>I'm a frontend developer and designer based in the Tampa Bay.</p>
+            <button>Know more</button>
+          </section>
+        </main>
+        <Footer/>
+      </>
+    );
+  }
 }
 
 export default App;
