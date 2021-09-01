41bec23699537f3d52a961a00dc795a02030d812
Unknown <tyler.simoni.8@gmail.com>
2021-08-31 21:33:06 -0400

    Adding config file

---
+++ content/meta/config.js
@@ -0,0 +1,40 @@
+module.exports = {
+  siteTitle: "Tyler Simoni | Portfolio", // <title>
+  shortSiteTitle: "Tyler Simoni | Portfolio", // <title> ending for posts and pages
+  siteDescription: "A portfolio of projects by Tyler Simoni",
+  siteUrl: "https://tylersimoni.me",
+  // pathPrefix: "",
+  siteImage: "preview.jpg",
+  siteLanguage: "en",
+
+  /* author */
+  authorName: "Tyler Simoni",
+  authorTwitterAccount: "tylersimoni",
+
+  /* info */
+  headerTitle: "Tyler Simoni",
+  headerSubTitle: "A portfolio for Tyler Simoni",
+
+  /* manifest.json */
+  manifestName: "Tyler Simoni | Portfolio",
+  manifestShortName: "TylerSimoni", // max 12 characters
+  manifestStartUrl: "/index.html",
+  manifestBackgroundColor: "white",
+  manifestThemeColor: "#494949",
+  manifestDisplay: "standalone",
+
+  // gravatar
+  // Use your Gravatar image. If empty then will use src/images/jpg/avatar.jpg
+  // Replace your email adress with md5-code.
+  // Example https://www.gravatar.com/avatar/g.strainovic@gmail.com ->
+  // gravatarImgMd5: "https://www.gravatar.com/avatar/1db853e4df386e8f699e4b35505dd8c6",
+  gravatarImgMd5: "",
+
+  // social
+  authorSocialLinks: [
+    { name: "github", url: "https://github.com/tsimmz" },
+    { name: "twitter", url: "https://twitter.com/tylersimoni" },
+    { name: "instagram", url: "http://instagram.com/t_simmz" },
+    { name: "linkedin", url: "https://linkedin.com/in/tylersimoni"}
+  ]
+};
