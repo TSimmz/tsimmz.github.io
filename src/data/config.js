35ef29bb7f811e53e0afe6a7e366cf1f48db11c2
Unknown <tyler.simoni.8@gmail.com>
2021-09-05 13:11:02 -0400

    Updating config and path to config

---
+++ src/data/config.js
@@ -0,0 +1,46 @@
+module.exports = {
+  defaultTitle: 'Tyler Simoni | Portfolio',
+  shortSiteTitle: 'Tyler Simoni | Portfolio', // <title> ending for posts and pages
+
+  logo: '',
+
+  author: 'Tyler Simoni',
+  legalName: 'Tyler Simoni',
+  url: 'https://tylersimoni.me',
+
+  defaultDescription: 'I’m Tyler and I’m a Frontend developer and designer!',
+
+  siteImage: 'preview.jpg',
+  siteLanguage: 'en',
+
+  socialLinks: {
+    twitter: 'https://twitter.com/tylersimoni',
+    github: 'https://github.com/tsimmz',
+    linkedin: 'https://linkedin.com/in/tylersimoni',
+    instagram: 'http://instagram.com/t_simmz',
+  },
+
+  googleAnalyticsID: '',
+
+  themeColor: '#494949',
+  backgroundColor: '#ffffff',
+
+  social: {
+    twitter: '@tylersimoni',
+    instagram: '@t_simmz',
+  },
+
+  address: {
+    city: 'North Redington Beach',
+    region: 'Florida',
+    country: 'United States',
+    zipCode: '33708',
+  },
+
+  contact: {
+    email: 'tyler.simoni.8@gmail.com',
+    phone: '724-372-9354',
+  },
+
+  foundingDate: '2021',
+};
