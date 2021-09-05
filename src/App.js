d3ba4f56ea5c067a3327b37a9197a9781f52d6b1
Unknown <tyler.simoni.8@gmail.com>
2021-09-05 12:00:43 -0400

    Adding Layout component

--- src/App.js
+++ src/App.js
@@ -3,20 +3,20 @@ import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import { Header } from "./components/theme";
 import { Home, Portfolio, About, Contact } from "./components/landing";
+import { Layout } from "./components/common/Layout/Layout";
 
 export const App = () => {
   return (
     <Router>
-      {/* <Layout/>
-            <Seo/>
-               */}
-      <Header />
-      <Switch>
-        <Route path="/" component={Home} />
-        <Route exact path="/portfolio" component={Portfolio} />
-        <Route exact path="/about" component={About} />
-        <Route exact path="/contact" component={Contact} />
-      </Switch>
+      <Layout>
+        <Header />
+        <Switch>
+          <Route path="/" component={Home} />
+          <Route exact path="/portfolio" component={Portfolio} />
+          <Route exact path="/about" component={About} />
+          <Route exact path="/contact" component={Contact} />
+        </Switch>
+      </Layout>
     </Router>
   );
 };
