0bf4093fa597a17fcc2c9cb32f62a386a0031722
Unknown <tyler.simoni.8@gmail.com>
2021-09-06 08:56:43 -0400

    Adding new theme object for provider

--- src/providers/ThemeProvider.jsx
+++ src/providers/ThemeProvider.jsx
@@ -1,6 +1,15 @@
-import React, { createContext, useState } from "react";
+import React, { createContext, useState } from 'react';
 
-export const ThemeContext = createContext("light");
+export const themes = {
+  light: {
+    background: '#ffffff',
+    lightColor: '#c7c7c7',
+    darkColor: '#494949',
+    accent: '#ee0000',
+  },
+};
+
+export const ThemeContext = createContext(themes.light);
 
 const ThemeProvider = ({ children }) => {
   const [theme, toggleTheme] = useState(false);
@@ -10,8 +19,7 @@ const ThemeProvider = ({ children }) => {
       value={{
         theme,
         toggleTheme,
-      }}
-    >
+      }}>
       {children}
     </ThemeContext.Provider>
   );
